41c68bbd1ea1c13233c92a4255d19a431776f9be
Aditya Mahajan <adityam172003@gmail.com>
2023-01-14 16:46:16 +0530

    final touch

--- MiddleWare/middleware.js
+++ MiddleWare/middleware.js
@@ -10,8 +10,9 @@ app.use(cookieparser());
 
 const Authentication= async (req,res,next)=>{
 try{
-   // res.header("Access-Control-Allow-Origin", "");
-    //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+   res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     const token=req.cookies.jwtoken;
    console.log("tkong generated is send to suthentication")
    console.log("tokken in authentication function ");
