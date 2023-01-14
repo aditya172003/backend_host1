29b68045ca367f7fe94b81155fcb3678568e6cfa
Aditya Mahajan <adityam172003@gmail.com>
2023-01-14 16:59:56 +0530

    changing position of credentials

--- server/auth.js
+++ server/auth.js
@@ -152,6 +152,9 @@ router.post("/log",async(req,res)=>{
 
 // removed middle woare , Authentication
 router.get("/ab",Authentication,(req,res)=>{
+   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+   res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    console.log("request reached ")
       res.send(req.rootuser)
       console.log(req.rootuser);
